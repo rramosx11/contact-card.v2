9eba785297f3b46c9c1499d5e449d10b35f31ae3
rramosx11 <rodolforamosd11@gmail.com>
2022-10-13 03:13:44 -0400

    added idb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,12 +2,14 @@ import "./form.js";
 import "./submit.js";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { initdb } from "./database";
 
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
 window.addEventListener("load", function () {
+  initdb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
